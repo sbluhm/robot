9e207aba0db0ba0b3d9cae5f20d375e51e95431d
Stefan Bluhm <stefan.bluhm@clacee.eu>
2024-10-03 17:26:36 +0200

    Changed to systemd

--- install.sh
+++ install.sh
@@ -1,3 +1,26 @@
+# Installing ROS
+
+## Set locale
+if [[ $(locale | grep UTF-8) ]]; then
+apt update && sudo apt install locales
+locale-gen en_US en_US.UTF-8
+update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+fi
+
+## Setup sources
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+## Install
+sudo apt update
+sudo apt upgrade
+#sudo apt install ros-humble-ros-base
+#sudo apt install ros-dev-tools
+
 #/boot/config.txt
 # hdmi_force_hotplug=1
 # apt update
@@ -8,5 +31,11 @@
 # Enable i2c
 #. raspi-config nonint
 #do_i2c 0
-cp /root/robot/os/90-update.sh /lib/dhcpcd/dhcpcd-hooks/
+#cp /root/robot/os/90-update.sh /lib/dhcpcd/dhcpcd-hooks/
+cp /root/robot/os/robot-update.service /etc/systemd/system
+systemctl daemon-reload
+systemctl enable robot-update.service 
 # this should be a dynamic link
+
+# Video
+pip install python3-opencv
