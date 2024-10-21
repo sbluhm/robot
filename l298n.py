4ed8e060ace76843fa81d0182edba597587de507
Stefan Bluhm <stefan.bluhm@clacee.eu>
2024-10-21 20:07:19 +0200

    Added l298n test script

---
+++ l298n.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as IO        
+import time                        
+IO.setwarnings(False)          
+IO.setmode (IO.BCM)         
+
+IO.setup(8,IO.OUT)        
+
+
+p = IO.PWM(8,1000)
+#IN1
+IO.setup(7, IO.OUT)
+IO.output(7, False)
+#IN2
+IO.setup(1, IO.OUT)
+IO.output(1, False)
+
+
+p.start(0)
+while 1:                              
+    for x in range (00,30,5):                      
+        p.ChangeDutyCycle(x)               
+        print(x)
+        time.sleep(5)  
