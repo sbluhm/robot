4414ad0c4e1e6ba8793d0d28169bd4d17141fd28
Stefan Bluhm <stefan.bluhm@clacee.eu>
2024-11-20 17:23:47 +0100

    Added zk-bm1 test

---
+++ testscripts/zk-bm1.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as IO        
+import time                        
+IO.setwarnings(False)          
+IO.setmode (IO.BCM)         
+
+PWM_FWD_PIN = 7
+PWM_REV_PIN = 9
+
+IO.setup(PWM_FWD_PIN,IO.OUT)
+IO.setup(PWM_REV_PIN,IO.OUT)
+p = IO.PWM(PWM_FWD_PIN,10000)
+o = IO.PWM(PWM_REV_PIN,10000)
+
+
+p.start(0)
+o.start(0)
+while 1:                              
+    for x in range (00,30,5):                      
+        p.ChangeDutyCycle(x)               
+        print(x)
+        time.sleep(5)  
+
+
+
