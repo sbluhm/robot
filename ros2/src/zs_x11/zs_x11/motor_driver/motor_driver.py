ac7f594cff0dd8543fbb03444e0c143d0c6d64f4
Stefan Bluhm <stefan.bluhm@clacee.eu>
2025-04-16 19:53:22 +0200

    half speed on full rotation

--- ros2/src/zs_x11/zs_x11/motor_driver/motor_driver.py
+++ ros2/src/zs_x11/zs_x11/motor_driver/motor_driver.py
@@ -68,8 +68,8 @@ class MotorDriver:
     def vdrive(self,y,x):
         scale = self.scale_speed
         if y == 0:         # voller einschlag falls nur links/rechts betätigt wird
-            l = x 
-            r = -x
+            l = x*0.5
+            r = -x*0.5
         else:
             if x >= 0:
                 l = y
