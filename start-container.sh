4bbac19d8465663f0c8b1eb1ea21fda560d9f714
Stefan Bluhm <stefan.bluhm@clacee.eu>
2025-04-17 14:00:43 +0200

    Optimised and added ROS version

--- start-container.sh
+++ start-container.sh
@@ -1,16 +1,17 @@
-# Dummy lsm6ds3
-if [[ `uname -m` == "x86_64" ]]; then 
-sudo modprobe i2c-dev
-sudo modprobe i2c-stub chip_addr=0x69
-# i2cdetect -l
-  sudo docker ps | grep ros_docker
-  if [ $? -gt 0 ]; then
-    echo "Starting a new container..."
-    sudo docker run -it --net=host --privileged --env="DISPLAY=$DISPLAY" --volume="${XAUTHORITY}:/root/.Xauthority" --hostname=ros2-$(hostname) ros_docker
+ROS_DISTRO=jazzy
+sudo docker ps | grep ros_docker_${ROS_DISTRO}
+if [ $? -gt 0 ]; then
+  echo "Starting a new container..."
+  if [[ `uname -m` == "x86_64" ]]; then 
+    # Dummy lsm6ds3
+    sudo modprobe i2c-dev
+    sudo modprobe i2c-stub chip_addr=0x69
+    # i2cdetect -l
+    sudo docker run -it --net=host --privileged --env="DISPLAY=$DISPLAY" --volume="${XAUTHORITY}:/root/.Xauthority" --hostname=ros2-$(hostname) ros_docker_${ROS_DISTRO}
   else
-    echo "Connecting to existing container..."
-    sudo docker exec -it `sudo docker ps | grep ros_docker | sed 's/ .*//'` bash
+    sudo docker run -it --net=host --hostname=ros2-$(hostname) --privileged ros_docker_ROS_${DISTRO}
   fi
 else
-  sudo docker run -it --net=host --hostname=ros2-$(hostname) --privileged ros_docker
+  echo "Connecting to existing container..."
+  sudo docker exec -it `sudo docker ps | grep ros_docker | sed 's/ .*//'` bash
 fi
