7904ccf16ecaa8510b0d3eee75008f21f71820ec
Stefan Bluhm <stefan.bluhm@clacee.eu>
2025-04-17 21:43:35 +0200

    Updated camera settings

--- ros2/src/robot_launcher/launch/robot_launch.py
+++ ros2/src/robot_launcher/launch/robot_launch.py
@@ -5,11 +5,14 @@ import launch_ros.actions
 import os
 
 def generate_launch_description():
-    config = os.path.join(
+    drive_config = os.path.join(
         get_package_share_directory('robot_launcher'),
         'config',
         'drivespecs.yaml')
-
+    c920_config = os.path.join(
+        get_package_share_directory('robot_launcher'),
+        'config',
+        'v4l2_camera.yaml')
     return launch.LaunchDescription([
         launch_ros.actions.Node(
             package='status_led',
@@ -23,7 +26,7 @@ def generate_launch_description():
             package='zs_x11',
             executable='zs_x11',
             name='zs_x11',
-            parameters=[config]),
+            parameters=[drive_config]),
         launch_ros.actions.Node(
             package='l298n',
             executable='l298n',
@@ -36,16 +39,6 @@ def generate_launch_description():
             package='v4l2_camera',
             executable='v4l2_camera_node',
             name='v4l2_camera',
-            arguments=['-p', 'image_size:="[640,480]"',
-                '-p', 'camera_frame_id:=camera_optical_link',
-                '-p', 'brightness:=128'
-                '-p', 'contrast:=128'
-                '-p', 'saturation:=128'
-                '-p', 'gain:=0'
-                '-p', 'white_balance_temperature:=4000'
-                '-p', 'sharpness:=128'
-                '-p', 'backlight_compensation:=0'
-                '-p', 'focus_absolute:=0'
-                '-p', 'poom_absolute:=100']),
+            parameters=[c920_config]),
   ])
 
