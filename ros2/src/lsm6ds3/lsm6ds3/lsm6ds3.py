b5ca0b880e92d501cdbb8f48694923065372741d
Stefan Bluhm <stefan.bluhm@clacee.eu>
2025-04-18 09:16:27 +0200

    hardcoded publish frequency as temporary workaround

--- ros2/src/lsm6ds3/lsm6ds3/lsm6ds3.py
+++ ros2/src/lsm6ds3/lsm6ds3/lsm6ds3.py
@@ -31,7 +31,8 @@ class ImuDriverROSWrapper(Node):
         gyroRange = self.get_parameter('~gyro_range').get_parameter_value().integer_value
         accelerometerRange = self.get_parameter('~accel_range').get_parameter_value().integer_value
 # division by 0
-        publish_ims_frequency = 1.0/self.get_parameter('~gyro_range').get_parameter_value().integer_value
+#        publish_ims_frequency = 1.0/self.get_parameter('~gyro_range').get_parameter_value().integer_value
+        publish_ims_frequency 1.0
   
         # LPF1_BW_SEL, FTYPE
         dlpfBandwidth = self.get_parameter('~dlpf_bandwidth').get_parameter_value().integer_value
