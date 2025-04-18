a23680c8a6d06ca803c507e2c488e7476522c126
Stefan Bluhm <stefan.bluhm@clacee.eu>
2025-04-18 08:40:22 +0200

    updated battery management node name

--- ros2/src/bluhmbot/setup.py
+++ ros2/src/bluhmbot/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'bluhmbot'
@@ -10,17 +12,19 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name,  'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='root@todo.todo',
+    maintainer='Stefan Bluhm',
+    maintainer_email='bluhmbot@clacee.eu',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'battery_management = battery_management.battery_management:main'
+            'battery_management_node = battery_management.battery_management:main'
         ],
     },
 )
