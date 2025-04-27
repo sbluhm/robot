35076fa36482ce95a0df3796ed751ee97a32f815
Stefan Bluhm <stefan.bluhm@clacee.eu>
2025-04-27 21:38:20 +0200

    added calibrate script

---
+++ ros2/src/bluhmbot/launch/calibrate_launch.py
@@ -0,0 +1,47 @@
+from ament_index_python.packages import get_package_share_directory
+
+import launch
+import launch_ros.actions
+import os
+
+def generate_launch_description():
+    c920_config = os.path.join(
+        get_package_share_directory('bluhmbot'),
+        'config',
+        'v4l2_camera.yaml')
+    drive_config = os.path.join(
+        get_package_share_directory('bluhmbot'),
+        'config',
+        'drivespecs.yaml')
+    joystick_config = os.path.join(
+        get_package_share_directory('bluhmbot'),
+        'config',
+        'joystick.yaml')
+    return launch.LaunchDescription([
+        launch_ros.actions.Node(
+            package='bluhmbot',
+            executable='twist_to_motors_node',
+            name='twist_to_motors_node',
+            parameters=[drive_config]),
+        launch_ros.actions.Node(
+            package='bluhmbot',
+            executable='pid_velocity_node',
+            name='left_pid_velocity',
+            parameters=[drive_config]),
+        launch_ros.actions.Node(
+            package='bluhmbot',
+            executable='pid_velocity_node',
+            name='right_pid_velocity',
+            parameters=[drive_config]),
+        launch_ros.actions.Node(
+            package='zs_x11',
+            executable='zs_x11',
+            name='left_wheel',
+            parameters=[drive_config]),
+        launch_ros.actions.Node(
+            package='zs_x11',
+            executable='zs_x11',
+            name='right_wheel',
+            parameters=[drive_config]),
+  ])
+
