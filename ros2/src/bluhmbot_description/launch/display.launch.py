aecfbda5d1eebc6c8df140c64e2aa9b83128cbc7
Stefan Bluhm <stefan.bluhm@clacee.eu>
2025-05-03 10:07:40 +0200

    baselined bluhmbot description package

--- ros2/src/bluhmbot_description/launch/display.launch.py
+++ ros2/src/bluhmbot_description/launch/display.launch.py
@@ -9,15 +9,15 @@ from launch_ros.actions import Node
 from ros_gz_bridge.actions import RosGzBridge
 from ros_gz_sim.actions import GzServer
 
-
 def generate_launch_description():
-    pkg_share = get_package_share_directory('sam_bot_description')
+    pkg_share = get_package_share_directory('bluhmbot_description')
+    default_rviz_config_path = os.path.join(pkg_share, 'rviz', 'config.rviz')
+    bridge_config_path = os.path.join(pkg_share, 'config', 'bridge_config.yaml')
+    world_path = os.path.join(pkg_share, 'world', 'my_world.sdf')
+
     ros_gz_sim_share = get_package_share_directory('ros_gz_sim')
     gz_spawn_model_launch_source = os.path.join(ros_gz_sim_share, "launch", "gz_spawn_model.launch.py")
-    default_model_path = os.path.join(pkg_share, 'src', 'description', 'sam_bot_description.sdf')
-    default_rviz_config_path = os.path.join(pkg_share, 'rviz', 'config.rviz')
-    world_path = os.path.join(pkg_share, 'world', 'my_world.sdf')
-    bridge_config_path = os.path.join(pkg_share, 'config', 'bridge_config.yaml')
+    default_model_path = os.path.join(pkg_share, 'src', 'description', 'bluhmbot_description.sdf')
 
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
@@ -44,50 +44,22 @@ def generate_launch_description():
         create_own_container='False',
         use_composition='True',
     )
-    camera_bridge_image = Node(
-        package='ros_gz_image',
-        executable='image_bridge',
-        name='bridge_gz_ros_camera_image',
-        output='screen',
-        parameters=[{'use_sim_time': True}],
-        arguments=['/depth_camera/image'],
-    )
-    camera_bridge_depth = Node(
-        package='ros_gz_image',
-        executable='image_bridge',
-        name='bridge_gz_ros_camera_depth',
-        output='screen',
-        parameters=[{'use_sim_time': True}],
-        arguments=['/depth_camera/depth_image'],
-    )
     spawn_entity = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(gz_spawn_model_launch_source),
         launch_arguments={
             'world': 'my_world',
             'topic': '/robot_description',
-            'entity_name': 'sam_bot',
-            'z': '0.65',
+            'entity_name': 'bluhmbot',
         }.items(),
     )
-    robot_localization_node = Node(
-        package='robot_localization',
-        executable='ekf_node',
-        name='ekf_node',
-        output='screen',
-        parameters=[os.path.join(pkg_share, 'config/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}],
-    )
-
     return LaunchDescription([
         DeclareLaunchArgument(name='model', default_value=default_model_path, description='Absolute path to robot model file'),
         DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path, description='Absolute path to rviz config file'),
         DeclareLaunchArgument(name='use_sim_time', default_value='True', description='Flag to enable use_sim_time'),
-        ExecuteProcess(cmd=['gz', 'sim', '-g'], output='screen'),
         robot_state_publisher_node,
         rviz_node,
+        ExecuteProcess(cmd=['gz', 'sim', '-g'], output='screen'),
         gz_server,
         ros_gz_bridge,
-        camera_bridge_image,
-        camera_bridge_depth,
         spawn_entity,
-        robot_localization_node,
     ])
