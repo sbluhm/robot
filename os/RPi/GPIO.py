f12a4f911a9f6f79a9f3bdabfa1628b1b6308f29
Stefan Bluhm <stefan.bluhm@clacee.eu>
2025-05-14 20:12:28 +0200

    Enabled multiple interrupt pins

--- os/RPi/GPIO.py
+++ os/RPi/GPIO.py
@@ -3,20 +3,29 @@ from threading import Thread
 
 RISING=True
 
-_current_dutycycle=0
+_current_dutycycle_left = 0
+_current_dutycycle_right = 0
 
 def _dummy_interrupt(callback, pin):
     global _current_dutycycle
     _MOTOR_ROC = 32.2418230613342
-    _MOTOR_SHIFT = -0.155436252405753
-    _MIN_POWER = 4.366
+#    _MOTOR_SHIFT = -0.155436252405753
+#    _MIN_POWER = 4.366
+    _MOTOR_SHIFT = 0.0
+    _MIN_POWER = 0.0
 
     while True:
-        if _current_dutycycle >= _MIN_POWER:
-            sleep(1/(( _current_dutycycle / _MOTOR_ROC - _MOTOR_SHIFT) * 90))
-            callback(pin)
+        if pin == 16:
+            dutycycle = _current_dutycycle_left
+        elif pin == 19:
+            dutycycle = _current_dutycycle_right
         else:
-            sleep(0.1)
+            raise Exception("No Interrupt Pin selected in _dummy_interrupt")
+
+        if dutycycle >= _MIN_POWER:
+            sleep(1/(( dutycycle / _MOTOR_ROC - _MOTOR_SHIFT) * 90))
+            callback(pin)
+        sleep(0.1)
 
 def BCM():
     return True
@@ -45,12 +54,17 @@ def add_event_detect(pin, edge,callback, bouncetime=10):
     dummy_interrupt_thread.start()
 
 class PWM():
-    def __init__(self,dummy1, dummy2):
+    def __init__(self,pwm_pin, frequency):
+        self.PWM_PIN = pwm_pin
         return None
     def start(self, dummy):
         return True
     def ChangeDutyCycle(self, dutycycle):
-        global _current_dutycycle
-        _current_dutycycle=dutycycle
+        if self.PWM_PIN == 12:
+            global _current_dutycycle_right
+            _current_dutycycle_right = dutycycle
+        if self.PWM_PIN == 12:
+            global _current_dutycycle_left
+            _current_dutycycle_left = dutycycle
         return True
 
