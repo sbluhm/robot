a956814971fba00f2ef17a8ed3610f768687213f
Stefan Bluhm <stefan.bluhm@clacee.eu>
2025-05-14 19:29:28 +0200

    updated nav2 script to start only navigation

--- os/nav2.sh
+++ os/nav2.sh
@@ -1,1 +1,2 @@
-ros2 launch nav2_bringup bringup_launch.py use_sim_time:=False autostart:=False map:=/root/robot/ros2/src/bluhmbot/maps/tb3_sandbox.yaml
+ros2 launch nav2_bringup navigation_launch.py
+#ros2 launch nav2_bringup bringup_launch.py use_sim_time:=False autostart:=False map:=/root/robot/ros2/src/bluhmbot/maps/tb3_sandbox.yaml
