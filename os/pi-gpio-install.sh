8ef1d1ff283b9698ee42a71428c97ee36a726715
Stefan Bluhm <stefan.bluhm@clacee.eu>
2025-05-18 13:26:36 +0200

    added pigpio to default image

--- os/pi-gpio-install.sh
+++ os/pi-gpio-install.sh
@@ -1,8 +1,26 @@
-apg-get install ros-jazzy-ros2-control ros-jazzy-ros2-controllers
-apt-get install unzip
-cd /tmp
-wget https://github.com/joan2937/pigpio/archive/master.zip
-unzip master.zip
+#https://github.com/Milliways2/pi-gpio/blob/main/examples/setGPIO.c
+#https://abyz.me.uk/rpi/pigpio/download.html
+
+apt-get install -y ros-jazzy-ros2-control ros-jazzy-ros2-controllers ros-jazzy-ros2-control-cmake
+apt-get install -y unzip
+cd ~/
+curl -O -J -L -s https://github.com/joan2937/pigpio/archive/master.zip
+unzip pigpio-master.zip
 cd pigpio-master
 make
-sudo make install
+make install
+#rm -Rf pigpio-master*
+#apt-get remove unzip
+
+
+
+
+cd 
+git clone https://github.com/ros-controls/ros2_control_demos   
+cd ros2_control_demos/ros2_control_demo_description
+colcon build; . install/setup.bash
+cd ../example_10
+
+
+
+
