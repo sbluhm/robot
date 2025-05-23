532a94f181339021abc9ad9a4ebb4cb7c010f2f8
Stefan Bluhm <stefan.bluhm@clacee.eu>
2025-05-23 16:09:54 +0200

    attempt as static function

--- ros2/src/example_2/hardware/include/ros2_control_demo_example_2/zs_x11_driver.hpp
+++ ros2/src/example_2/hardware/include/ros2_control_demo_example_2/zs_x11_driver.hpp
@@ -14,22 +14,29 @@ class ZS_X11_Driver
 
 public:
 
-  ZS_X11_Driver() = default;
+  ZS_X11_Driver() {
+    instance_ = this;
+  }
+
   int tick_counter_l = 0;
   int tick_counter_r = 0;
   int _direction_l = 1;
   int _direction_r = 1;
 
-  void leftSpeedPulseCallback()
+  static void leftSpeedPulseCallback()
   {
-	  this->tick_counter_l += this->_direction_l;
-          std::cout << "Left Interrupt. Tick counter: " << this->tick_counter_l << std::endl;
+    if (instance_) {
+	  instance_->tick_counter_l += instance_->_direction_l;
+          std::cout << "Left Interrupt. Tick counter: " << instance_->tick_counter_l << std::endl;
+    }
   }
 
-  void rightSpeedPulseCallback()
+  static void rightSpeedPulseCallback()
   {
-          this->tick_counter_r += this->_direction_r;
-          std::cout << "Right Interrupt. Tick counter: " << this->tick_counter_r << std::endl;
+    if (instance_) {
+          instance_->tick_counter_r += instance_->_direction_r;
+          std::cout << "Right Interrupt. Tick counter: " << instance_->tick_counter_r << std::endl;
+    }
   }
 
 
@@ -83,10 +90,10 @@ public:
 
     if( left < 0 ) {
         gpioWrite(6, PI_ON);
-	this->_direction_l = -1;
+	_direction_l = -1;
     } else {
         gpioWrite(6, PI_OFF);
-	this->_direction_l = 1;
+	_direction_l = 1;
     }
     power = 0;
     if( abs(left*RADIUS) >= MIN_SPEED ) {
@@ -97,10 +104,10 @@ public:
 
     if( right > 0 ) {
         gpioWrite(5, PI_ON);
-        this->_direction_r = 1;
+        _direction_r = 1;
     } else {
         gpioWrite(5, PI_OFF);
-        this->_direction_r = -1;
+        _direction_r = -1;
     }
     power = 0;
     if( abs(right*RADIUS) >= MIN_SPEED ) {
@@ -110,8 +117,13 @@ public:
     std::cout << "PIN12 PWM: " << PWM_FREQUENCY << "Input: " << right << "Power: " << power << " Result: " << result << std::endl;
   }
 
+  ~ZS_X11_Driver() {
+    disconnect();
+  }
+
 private:
     int timeout_ms_;
+  static ZS_X11_Driver* instance_;
 };
 
 #endif // ROS2_CONTROL_DEMO_EXAMPLE_2__ZS_X11_DRIVER_HPP
