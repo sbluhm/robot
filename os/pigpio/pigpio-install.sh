ba61417bdacbbad0a42490549230c491bf3903b4
Stefan Bluhm <stefan.bluhm@clacee.eu>
2025-05-26 07:31:07 +0200

    Allow non-git install

--- os/pigpio/pigpio-install.sh
+++ os/pigpio/pigpio-install.sh
@@ -7,6 +7,7 @@ curl -O -J -L -s https://github.com/joan2937/pigpio/archive/${ref}.tar.gz
 tar xf pigpio-${ref}.tar.gz
 ln -s pigpio-${ref} pigpio
 cd pigpio
-patch < ~/robot/os/pigpio/ISRfix613.patch
+curl -O -J -L -s https://github.com/sbluhm/robot/raw/refs/heads/master/os/pigpio/ISRfix613.patch
+patch < ISRfix613.patch
 make
 make install
--- testscripts/irq/gpio_test.cpp
+++ testscripts/irq/gpio_test.cpp
@@ -8,9 +8,9 @@ void testCallback(int gpio, int level, uint32_t tick) {
 int main() {
     if (gpioInitialise() < 1) return 1;
 
-    std::cout << "Set Mode: " << gpioSetMode(17, PI_INPUT) << std::endl;
-//    gpioSetPullUpDown(17, PI_PUD_UP);
-    std::cout << "Set ISR: " << gpioSetISRFunc(17, RISING_EDGE, 0, testCallback) << std::endl;
+    std::cout << "Set Mode: " << gpioSetMode(16, PI_INPUT) << std::endl;
+//    gpioSetPullUpDown(16, PI_PUD_UP);
+    std::cout << "Set ISR: " << gpioSetISRFunc(16, RISING_EDGE, 0, testCallback) << std::endl;
     std::cout << "Waiting for interrupt..." << std::endl;
     while (true) gpioDelay(100001);  // Sleep 100ms
 
