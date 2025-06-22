2133d44628fc04c080df645537d7a650cfe9b459
Stefan Bluhm <stefan.bluhm@clacee.eu>
2025-06-22 08:10:25 +0200

    Enhanced rpi_pwm script to accept branch parameter

--- os/rpi_pwm/rpi_pwm-install.sh
+++ os/rpi_pwm/rpi_pwm-install.sh
@@ -1,7 +1,14 @@
+set -e
+BRANCH=master
+if [ -n "$1" ]
+  then
+    BRANCH=$1
+fi
+
 cd /usr/local/src
-git clone https://github.com/berndporr/rpi_pwm.git
+git clone https://github.com/berndporr/rpi_pwm.git ||:
 cd rpi_pwm
-curl -O -J -L -s https://github.com/sbluhm/robot/raw/refs/heads/pi5/os/rpi_pwm/chip.patch
+curl -O -J -L -s https://github.com/sbluhm/robot/raw/refs/heads/${BRANCH}/os/rpi_pwm/chip.patch
 patch < chip.patch
 cmake .
 make
