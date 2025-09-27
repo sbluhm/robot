6edfa8a0c7630cc38b3916c52fa104cfeed273ed
Stefan Bluhm <stefan.bluhm@clacee.eu>
2025-09-27 19:34:18 +0200

    Added keyboard teleop package

--- install-jazzy.sh
+++ install-jazzy.sh
@@ -77,7 +77,7 @@ cat >> Dockerfile << EOF
 SHELL ["/bin/bash", "-c"]
 RUN apt-get -y update && apt-get -y upgrade && apt-get install -y --no-install-recommends \
     vim libgpiod-dev \
-    ros-${ROS_DISTRO}-joy ros-jazzy-teleop-twist-joy\
+    ros-${ROS_DISTRO}-joy ros-jazzy-teleop-twist-joy ros-jazzy-teleop-twist-keyboard \
     ros-${ROS_DISTRO}-ros2-control ros-${ROS_DISTRO}-ros2-controllers ros-${ROS_DISTRO}-ros2-control-cmake \
     v4l-utils \
     ros-${ROS_DISTRO}-v4l2-camera \
