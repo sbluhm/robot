63b68139adc43ccfbf746bd96e1a58320b21f1d3
Stefan Bluhm <stefan.bluhm@clacee.eu>
2025-10-20 21:31:54 +0200

    x

--- os/rviz.sh
+++ os/rviz.sh
@@ -1,1 +1,6 @@
+# Avoiding QStandardPaths: XDG_RUNTIME_DIR not set, defaulting to '/tmp/runtime-root' 
+export XDG_RUNTIME_DIR=/run/user/$(id -u)
+mkdir -p "$XDG_RUNTIME_DIR"
+chmod 700 "$XDG_RUNTIME_DIR"
+
 ros2 run rviz2 rviz2 -d $(ros2 pkg prefix bluhmbot)/share/bluhmbot/rviz/nav2_default_view.rviz 
